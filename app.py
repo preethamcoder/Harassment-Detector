05d83b627537e2cf2e6100061d0e0d7207971815
preethamcoder <preetham23415@gmail.com>
2023-04-08 18:20:30 -0400

    Fixed the issues with sliders

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 from flask import Flask, render_template, request
-# import numpy as np
-# import pickle
-# import nltk
-# nltk.download('stopwords')
-# from nltk.corpus import stopwords
-# from sklearn.feature_extraction.text import TfidfVectorizer
 import fasttext
 
 app = Flask(__name__)
 
-# with open('vect.pkl', 'rb') as f:
-# 	vect = pickle.load(f)
- 
 model = "samurai"
 def load_da_model():
 	global model
 	model = fasttext.load_model("trained.ftz")
-	# with open('sexual_assault.pkl', 'rb') as f:
-	# 	model = pickle.load(f)
-
 
 @app.route('/')
 def home():
@@ -32,17 +20,17 @@ def meth():
 def predict_da_text():
 	if request.method == 'POST':
 		stuff = request.form
-		# txt = vect.transform([stuff['inp']])
-		# total = np.sum(model.predict(txt).toarray())
-		# print(total)
-		predicted = model.predict(stuff['inp'].lower())
+		text = stuff['inp']
+		print(text)
+		predicted = model.predict(text.lower())
 		res = "hate" if predicted[0][0][-1]=='1' else "not hate"
 		if res == "hate":
 			verdict = "watch what you say, dumbass"
 		else:
 			verdict = "this is fine"
-	return render_template('pred.html', shit=stuff['inp'], pred=res, verdict=verdict)
+	return render_template('pred.html', shit=text, pred=res, verdict=verdict)
 
 if __name__ == '__main__':
 	load_da_model()
 	app.run(host='0.0.0.0', debug=True, port=5000)
+
